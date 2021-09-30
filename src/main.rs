5ec864d01fcefcbda1c21235836e2046d44628cb
Mark Prediger <mark.prediger@trinamix.de>
2021-09-30 20:11:01 +0200

    Improve init screen, loose screen

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,89 @@ extern crate piston_window;
 extern crate find_folder;
 
 use piston_window::*;
-use crate::ActiveScreen::{MainGame, InitScreen};
+use crate::ActiveScreen::{MainGame, InitScreen, LooseScreen};
+use piston_window::math::Scalar;
 
 enum ActiveScreen {
     MainGame(game::MainGame),
     InitScreen,
+    LooseScreen,
+}
+
+fn text_size<C>(
+    font_size: types::FontSize,
+    text: &str,
+    cache: &mut C,
+) -> Result<(Scalar, Scalar), C::Error>
+    where
+        C: character::CharacterCache,
+{
+    // TODO this assumes that text is ordered horizontal
+    let mut width: Scalar = 0.0;
+    let mut height: Scalar = 0.0;
+
+    for ch in text.chars() {
+        let character = cache.character(font_size, ch)?;
+
+        width += character.advance_width();
+        height = height.max( character.atlas_size[1] *2.0 - character.top());
+    }
+
+    Ok((width, height))
+}
+
+fn draw_text_lines<C, G>(color: types::Color,
+                         font_size: types::FontSize,
+                         lines: &[&str],
+                         area_size: (f64, f64),
+                         cache: &mut C,
+                         transform: math::Matrix2d,
+                         g: &mut G,
+) -> Result<(), C::Error>
+    where
+        C: character::CharacterCache,
+        G: Graphics<Texture=<C as character::CharacterCache>::Texture>
+{
+    let line_spacing_add = font_size as f64 * 0.25;
+
+    let mut sizes: Vec<(Scalar, Scalar)> = Vec::new();
+    let mut total_height = (lines.len()-1) as f64 * line_spacing_add;
+
+    for &line in lines {
+        let s = text_size(font_size, line, cache)?;
+        sizes.push(s);
+        total_height += s.1;
+    }
+
+    let mut y_pos = (area_size.1 - total_height) / 2.0 + sizes[0].1;
+
+    for (index, line) in lines.iter().enumerate() {
+        let s = sizes[index];
+
+        let x_pos = (area_size.0 - s.0) / 2.0;
+
+        text(color, font_size, line, cache,
+             transform.trans(x_pos, y_pos), g)?;
+
+        y_pos += s.1 + line_spacing_add;
+    }
+
+    Ok(())
+}
+
+fn draw_text_multiline<C, G>(color: types::Color,
+                             font_size: types::FontSize,
+                             text: &str,
+                             area_size: (f64, f64),
+                             cache: &mut C,
+                             transform: math::Matrix2d,
+                             g: &mut G,
+) -> Result<(), C::Error>
+    where
+        C: character::CharacterCache,
+        G: Graphics<Texture=<C as character::CharacterCache>::Texture>
+{
+    draw_text_lines(color, font_size, &text.lines().collect::<Vec<&str>>(), area_size, cache, transform, g)
 }
 
 fn main() {
@@ -30,8 +108,18 @@ fn main() {
     while let Some(e) = window.next() {
         match &mut active {
             MainGame(ref mut game) => {
+                if let Some(button) = e.press_args() {
+                    match button {
+                        Button::Keyboard(Key::Escape) => {
+                            active = InitScreen;
+                            continue;
+                        }
+                        _ => (),
+                    }
+                }
+
                 if !game.run(&mut window, &mut glyphs, e) {
-                    active = InitScreen;
+                    active = LooseScreen;
                 }
             }
             InitScreen => {
@@ -50,10 +138,40 @@ fn main() {
                 window.draw_2d(&e, |c, g, device| {
                     clear([0.95, 0.95, 0.95, 1.0], g);
 
-                    text([0.0, 0.0, 0.0, 1.0], 64, "Press enter", &mut glyphs,
-                         c.transform.trans(40.0, rect_size * field_size / 2.0), g).unwrap();
-                    text([0.0, 0.0, 0.0, 1.0], 64, "to start", &mut glyphs,
-                         c.transform.trans(40.0, rect_size * field_size / 2.0 + 50.0), g).unwrap();
+                    draw_text_multiline([0.0, 0.0, 0.0, 1.0], 64, "Snake Game", (rect_size * field_size, 100.0),
+                                        &mut glyphs, c.transform, g,
+                    ).unwrap();
+
+                    draw_text_multiline([0.0, 0.8, 0.0, 1.0], 48, "Press enter\nto start!", (rect_size * field_size, rect_size * field_size-100.0),
+                                        &mut glyphs, c.transform.trans(0.0, 100.0), g,
+                    ).unwrap();
+
+
+                    // Update glyphs before rendering.
+                    glyphs.factory.encoder.flush(device);
+                });
+            }
+            LooseScreen => {
+                if let Some(button) = e.press_args() {
+                    match button {
+                        Button::Keyboard(Key::Return) => {
+                            active = InitScreen;
+                        }
+                        Button::Keyboard(Key::Escape) => {
+                            return;
+                        }
+                        _ => (),
+                    }
+                }
+
+                window.draw_2d(&e, |c, g, device| {
+                    clear([0.95, 0.95, 0.95, 1.0], g);
+
+
+                    draw_text_multiline([0.8, 0.0, 0.0, 1.0], 48, "You lost!\n \nPress enter\nto return to\nmain screen.", (rect_size * field_size, rect_size * field_size),
+                                        &mut glyphs, c.transform, g,
+                    ).unwrap();
+
 
                     // Update glyphs before rendering.
                     glyphs.factory.encoder.flush(device);
